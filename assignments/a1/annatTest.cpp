ec065b45fd33dc7f294196ef0845e08041da4222
Axel Froborg <23397446+afroborg@users.noreply.github.com>
2022-06-15 15:00:08 +0200

    fix assignment one

--- assignments/a1/annatTest.cpp
+++ assignments/a1/annatTest.cpp
@@ -5,7 +5,7 @@ int main(){
 
   int a;
   int b = 12;
-  a = 0
+  a = 0; // there was a missing semicolon here causing the build to fail
 
   cout << "a = " << a << endl;
   cout << a+b << '\n' << "a-b = " << a-b << endl;
